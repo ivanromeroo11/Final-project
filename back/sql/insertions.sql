e85ecd419d6463a715fa29299711c800a10bcdc2
Ivan Romero <ivanromero5520@gmail.com>
2023-07-03 18:53:56 +0200

    Insert Ventilacion into Products

--- back/sql/insertions.sql
+++ back/sql/insertions.sql
@@ -54,6 +54,59 @@ INSERT INTO products (
 INSERT INTO products (
     name, brand, description, price, quantity, category
 )VALUES(
-    ''
+    'Corsair iCUE H150i ELITE LCD XT Kit de Refrigeración Líquida 360mm Negro', 'Corsair', 'El refrigerador líquido para CPU CORSAIR iCUE H150i ELITE LCD XT ofrece una refrigeración potente y personalizada para su CPU; consta de una pantalla LCD personalizada que le permite visualizar GIF, imágenes e información del sistema.', '300.99', 'En stock', 'Ventilacion'
 );
 
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'ASUS ROG RYUJIN II 240 ARGB Kit de Refrigeración Líquida con Pantalla LCD 3.5', 'Asus', 'Cooler CPU todo en uno ROG Ryujin II 240 con pantalla LCD de 3,5", ventilador integrado para la bomba y 2 ventiladores de radiador ROG ARGB de 120mm.', '280.00', 'En stock', 'Ventilacion' 
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+     'Tempest Fan 120mm Negro PWM', 'Tempest', 'Tempest Fan 120mm Negro PWM es un ventilador de 120mm con conector 4 pin PWM. Está diseñado con un 7 aspas curvas que incrementan el caudal de aire con una buena presión y una sonoridad baja para optimizar la refrigeración de tu PC.', '4.00', 'Agotandose', 'Ventilacion'
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'Tempest Zero II Pasta Térmica 1g 6.2W/mK', 'Tempest', '¡Bienvenidos al mundo del gaming con Tempest Zero II! Esta pasta térmica ha sido diseñada para los gamers más exigentes, aquellos que buscan sacar el máximo rendimiento de sus equipos y reducir la temperatura de los componentes durante largas sesiones de juego. Con una conductividad térmica de 6.2W/mK, Tempest Zero II ofrece una rápida disipación del calor, asegurando un mejor rendimiento y estabilidad de tu equipo. Además, su fórmula no conductora y no corrosiva la hace compatible con todo tipo de equipos ya sean portátiles o torres de pc, lo que la convierte en una opción segura y eficaz.', '2.00', 'Agotandose', 'Ventilacion' 
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'MSI MAG CoreLiquid 360R V2 Kit de Refrigeración Líquida ARGB', 'MSI', 'La nueva refrigeración líquida MAG CORELIQUID Series tiene todo lo que buscas, materiales de alta calidad que facilitan la disipación del calor de forma extremadamente efectiva. Cómo no, cuenta con iluminación ARGB y un bloque capaz de rotar hasta 270 grados. El diseño del bloque es único y asimétrico. Cada componente individual de MAG CORELIQUID Series ha sido diseñado con un único fin: Conseguir la mejor disipación.', '124.99', 'En stock', 'Ventilacion'
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'Corsair iCUE QL120 RGB PWM Single Pack Negro 120 mm', 'Corsair', 'Brinde a su PC una iluminación espectacular desde cualquier ángulo con un ventilador CORSAIR iCUE QL120 RGB PWM White, equipado con 34 LED RGB direccionables individualmente a través de cuatro lazos de luz distintos en una carcasa blanca brillante.', '49.00', 'En stock', 'Ventilacion'
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'Gigabyte Aorus ATC800 Ventilador CPU 120mm', 'Gigabyte', 'Con la placa base Aorus ATC800 y el especialista en tarjetas gráficas, Gigabyte presenta un enfriador de CPU silencioso y potente con iluminación LED RGB sincronizable.', '119.99', 'En stock', 'Ventilación' 
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'Gigabyte Aorus WaterForce X 360 Kit de Refrigeración Líquida', 'Gigabyte', 'AORUS WATERFORCE X 360, enfriador de líquido todo en uno con pantalla LCD circular, RGB Fusion 2.0 y ventiladores ARGB de 120 mm', '249.99', 'En stock', 'Ventilación'
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'MSI MEG CORELIQUID S360 Kit de Refrigeración Líquida', 'MSI', 'La serie MEG CORELIQUID S está aquí para proporcionar la máxima refrigeración silenciosa. Está equipado con el innovador ventilador silencioso gaming MEG SILENT GALE P12 y soporta el modo de ventilador a 0 rpm. Con la pantalla IPS de 2.4" podrás consultar información sobre hardware o gráficos y vídeos personalizables. La Serie S de MEG CORELIQUID desciende desde lo más alto y no se oye venir.', '300.99', 'En stock', 'Ventilación'
+);
+
+INSERT INTO products (
+    name, brand, description, price, quantity, category
+)VALUES(
+    'ASUS ROG Strix LC II 360 Kit de Refrigeración Líquida', 'Asus', 'El nuevo diseño del ROG Strix LC II 360 hace gala de la estética ROG más icónica. Las letras ROG grabadas en el lateral del radiador complementan el patrón iridiscente en los ventiladores. Y la cubierta de la bomba con iluminación Aura Sync irradia el nuevo logotipo de ROG en el color que elijas.', '350.00', 'En stock', 'Ventilación'
+);
